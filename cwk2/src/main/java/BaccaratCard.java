7a7f6e19eb695c324fdf9c7cbe770223b6ae7c58
IlanRV <146100248+IlanRV@users.noreply.github.com>
2024-04-11 17:38:53 +0000

    Notes were created on what each part of the code should do.

--- cwk2/src/main/java/BaccaratCard.java
+++ cwk2/src/main/java/BaccaratCard.java
@@ -7,31 +7,33 @@ public class BaccaratCard {
     
     public Rank getRank()
     {
-        return r;
+        return rank; // gets the number or JQKA of the cards return Rank 
     }
     
     public Suit getSuit()
     {
-        return s;
+        return suit; // gets its symbol Hearts, Spades, etc. return Suit 
     }
 
     public String toString()
     {
-        return string;
+        return string; // turns rank and suit into a two character string (first shows rank then suit).
     }
 
     public boolean equals(Object other)
     {
-        return equals;
+        return equals; // Shows the value of both the cards that were taken out ???  It should be possible to compare BaccaratCard objects in two ways, using the equals() and compareTo() methods.
     }
 
     public int compareTo(Card other)
     {
-        return compareTo;
+        return compareTo; // ???
     }
     
     public int value()
     {
-        return value;
+        return value; // return the value of both the card (ex: K+3 = 3, 2+5 = 7, K+Q = 0, etc)
     }
+
+// TO answer some of these questions the Card class was created check to help out your code. 
 }
--- cwk2/src/main/java/BaccaratHand.java
+++ cwk2/src/main/java/BaccaratHand.java
@@ -8,31 +8,33 @@ public class BaccaratHand {
     
     public BaccaratHand()
     {
-        break;
+        break; //should store the value obtained from the cards from BaccaratCard but starts as empty. 
     }
 
     public int size()
     {
-        return size;
+        return size; // should return the number of cards in the hand. 
     }
     
     public add(Card card)
     {
-        return add;
+        return add; // makes it possible to add a BACCARATCARD to a BACCARATHAND by calling -.add()- method. 
     }
 
     public int value()
     {
-        return value;
+        return value; // Return the points value of the hand (K+3=3...)
     }
 
     public boolean isNatural()
     {
-        return isNatural;
+        return isNatural; // Should return TRUE if the hand has two cards and (&&) a points value of 8-9 or else false. 
     }
 
     public String toString()
     {
-        return toString;
+        return toString; // Should return a string with two character representations of each card separated by a space from the other (7♠ 4♣).
     }
+
+    // TO answer some of these questions the CardCollection class was created check to help out your code. 
 }
