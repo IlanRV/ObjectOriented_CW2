3e75d8b65afbdc4cfb718e1e04f1ec9a52150098
IlanRV <146100248+IlanRV@users.noreply.github.com>
2024-04-14 17:34:40 +0000

    Finally all compiles

--- cwk2/src/main/java/BaccaratCard.java
+++ cwk2/src/main/java/BaccaratCard.java
@@ -1,43 +1,44 @@
 // TODO: Implement the BaccaratCard class in this file
 
-public class BaccaratCard {
-    
 
-    public BaccaratCard(Card.Suit s, Card.Rank r)
-    {
-        Card.rank = r;
-        Card.suit = s;
-    }
-    
-    public Card.Rank getRank()
-    {
-        return Card.rank; // gets the number or JQKA of the cards return Rank 
-    }
-    
-    public Card.Suit getSuit()
-    {
-        return Card.suit; // gets its symbol Hearts, Spades, etc. return Suit 
+public class BaccaratCard extends Card {
+ 
+    private Card.Rank rank; 
+    private Card.Suit suit;
+
+    public BaccaratCard(Rank r, Suit s) {
+        super(r, s);
+        suit = s;
+        rank = r;
     }
 
-    public String toString()
-    {
+    public Rank getRank() {
+
+        return null; // gets the number or JQKA of the cards return Rank
+    }
+
+    public Suit getSuit() {
+        return null; // gets its symbol Hearts, Spades, etc. return Suit
+    }
+
+    public String toString() {
         return null; // turns rank and suit into a two character string (first shows rank then suit).
     }
 
-    public boolean equals(Object other)
-    {
-        return false; // Shows the value of both the cards that were taken out ???  It should be possible to compare BaccaratCard objects in two ways, using the equals() and compareTo() methods.
+    public boolean equals(Object other) {
+        return true; // Shows the value of both the cards that were taken out ??? It should be
+                     // possible to compare BaccaratCard objects in two ways, using the equals() and
+                     // compareTo() methods.
     }
 
-    public int compareTo(Card other)
-    {
+    public int compareTo(Card other) {
         return 0; // ???
     }
-    
-    public int value()
-    {
+
+    public int value() {
         return 0; // return the value of both the card (ex: K+3 = 3, 2+5 = 7, K+Q = 0, etc)
     }
 
-// TO answer some of these questions the Card class was created check to help out your code. 
+    // TO answer some of these questions the Card class was created check to help
+    // out your code.
 }
