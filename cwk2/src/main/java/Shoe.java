641a47d8c3accaec9b8f339ea8a76d381b060d6c
IlanRV <146100248+IlanRV@users.noreply.github.com>
2024-04-29 14:25:20 +0000

    shoe almost done

--- cwk2/src/main/java/Shoe.java
+++ cwk2/src/main/java/Shoe.java
@@ -1,28 +1,61 @@
 // TODO: Implement the Shoe class in this file
+import java.util.Collections;
 
 public class Shoe {
+    private BaccaratCard[] cards; //stores card in shoe.
+    private int size;  // Keeps track of number of cards.
+
     public Shoe(int decks)
     {
-        // Must have a -constructor- in which the number of decks of cards is specified as a parameter (number of decks from 6 or 8) any other value = CardExeption.
-        // the constructor of shoe should ensure that a shoe stores the specified number of complete decks of BaccaratCard objects. (a full deck = 52 ordered first by suit and then rank !! NO OTHER ORDERS ALLOWED !!).
-    
+        if (decks !=6 && decks !=8) {
+            throw new CardException("Number of decks must be 6 or 8.");
+        } 
+
+        int totalCards = decks * 52; //calculates the total number of cards needed.
+        
+        cards = new BaccaratCard[totalCards]; //starts storing cards.
+
+        //starts creating complete decks.
+
+        for (int i = 0; i < decks; i++){
+            for (Card.Rank rank : Card.Rank.values()) {
+                for (Card.Suit suit : Card.Suit.values()) {
+                    cards[size++] = new BaccaratCard(rank, suit);
+                }
+            }
+        }
     }
 
     public int size()
     {
-        return 0; // return the number of cards in the shoe.
+        return size; // return the number of cards in the shoe.
     }
 
-    public int shuffle()
-    {
-        return 0; // re orders the cards of the shoe randomly use "COLLECTIONS utility class" from "JAVA.UTIL" package. 
+    public int shuffle() {
+        for (int i = cards.length - 1; i > 0; i--) {
+            // Generate a random index between 0 and i (inclusive)
+            int j = (int) (Math.random() * (i + 1));
+    
+            // Swap the cards at indices i and j
+            BaccaratCard temp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = temp;
+        }
+    
+        return 0; // Placeholder return value
     }
 
     public Card deal()
     {
-        return null; // removes the first stored card and returns it to the caller. if deals from empty shoe = CardExeption.  
+        if (size == 0) {
+            throw new CardException("Error dealing from an empty shoe.");
+        }
+        
+        Card dealtCard = cards[--size];
+        cards[size] = null; 
+        
+        return dealtCard; // removes the first stored card and returns it to the caller. if deals from empty shoe = CardExeption.  
     }
-    
     
     // TO answer some of these questions the CardCollection class was created check to help out your code. 
 
